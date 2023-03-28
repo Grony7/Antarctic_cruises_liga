a81fd4368e4989f0267ac57fe011d2f3e3c38a59
Grony <gr0ny.0017@gmail.com>
2023-03-28 15:46:09 +0300

    Ссылки и шрифты

--- source/js/modules/nav.js
+++ source/js/modules/nav.js
@@ -1,5 +1,8 @@
 const menu = document.querySelector('[data-menu="menu"]');
 const menuToggle = document.querySelector('[data-menu="button"]');
+const menuList = menu.querySelector('[data-menu="list"]');
+const menuLinks = menuList.querySelectorAll('a')
+
 
 
 export const menuStatus = () => {
@@ -8,4 +11,16 @@ export const menuStatus = () => {
       menu.classList.toggle('is-open')
     })
   }
+
+  if (menuLinks && menu) {
+    menuLinks.forEach((link) => {
+      link.addEventListener("click", () => {
+        if(menu.classList.contains('is-open')) {
+          menu.classList.remove('is-open');
+        }
+      });
+    });
+  }
 }
+
+
