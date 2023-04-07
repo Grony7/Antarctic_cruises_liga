24222560b303c59342ade53bb50856e83ebdffd8
Grony <gr0ny.0017@gmail.com>
2023-04-07 22:05:31 +0300

    fix

--- source/js/modules/nav.js
+++ source/js/modules/nav.js
@@ -1,22 +1,46 @@
 const menu = document.querySelector('[data-menu="menu"]');
 const menuToggle = document.querySelector('[data-menu="button"]');
+const body = document.querySelector("body");
+const menuOverlay = document.querySelector('[data-menu="overlay"]');
 const menuList = menu.querySelector('[data-menu="list"]');
 const menuLinks = menuList.querySelectorAll('a');
+
+const openMenu = () => {
+  menu.classList.add('is-open');
+  body.classList.add('menu-open');
+}
+
+const closeMenu = () => {
+  menu.classList.remove('is-open');
+  body.classList.remove('menu-open');
+}
 
 export const menuStatus = () => {
   if (menu && menuToggle) {
     menuToggle.addEventListener('click', () =>{
-      menu.classList.toggle('is-open');
+      if (menu.classList.contains('is-open')) {
+        closeMenu();
+      } else {
+        openMenu();
+      }
     });
   }
 
   if (menuLinks && menu) {
     menuLinks.forEach((link) => {
-      link.addEventListener('"click"', () => {
+      link.addEventListener('click', () => {
         if (menu.classList.contains('is-open')) {
-          menu.classList.remove('is-open');
+          closeMenu();
         }
       });
     });
   }
+
+  if (menuOverlay) {
+    menuOverlay.addEventListener('click', () => {
+      closeMenu();
+    });
+  }
 };
+
+
